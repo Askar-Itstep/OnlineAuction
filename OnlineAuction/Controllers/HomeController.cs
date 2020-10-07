0f4d68ba64b1f4c423f8943e06ae7500bde14788
askar76-r@yandex.ru <askar76-r@yandex.ru>
2020-10-07 10:45:11 +0600

    many fix for pushsender

--- OnlineAuction/Controllers/HomeController.cs
+++ OnlineAuction/Controllers/HomeController.cs
@@ -25,28 +25,60 @@ namespace OnlineAuction.Controllers
             var accountId = Session["accountId"] ?? 0;
             if ((int)accountId != 0) {
                 AccountBO account = DependencyResolver.Current.GetService<AccountBO>().Load((int)accountId);
-                List<RoleAccountLinkBO> rolesAccount= DependencyResolver.Current.GetService<RoleAccountLinkBO>()
-                    .LoadAll().Where(r=>r.AccountId==(int)accountId).ToList();
+                List<RoleAccountLinkBO> rolesAccount = DependencyResolver.Current.GetService<RoleAccountLinkBO>()
+                    .LoadAll().Where(r => r.AccountId == (int)accountId).ToList();
                 var roleAdmin = rolesAccount.FirstOrDefault(r => r.Role.RoleName.Contains("admin"));
                 if (account != null && roleAdmin == null) {
                     Account = mapper.Map<AccountVM>(account);
-                    var sender = PushSender.Instance;
+                    var sender = PushSender.InstanceClient;
                     sender.Account = Account;
-                    await sender.SendMessage(string.Format("А у нас новый участник: {0}", Account.FullName));
+                    await sender.SendHello(string.Format("А у нас новый участник: {0}", Account.FullName));
                 }
             }
             return View();
         }
         public ActionResult Chat()
         {
-                var accountId = Session["accountId"] ?? 0;
-                if ((int)accountId == 0) {
-                    return RedirectToAction("Login", "Accounts");
-                }
-                ViewBag.User = Account;
-                return View("Partial/_ChatPartialView");  
+            var accountId = Session["accountId"] ?? 0;
+            if ((int)accountId == 0) {
+                return RedirectToAction("Login", "Accounts");
+            }
+            ViewBag.User = Account;
+            return View("Partial/_ChatPartialView");
         }
-       
+        public async Task<ActionResult> ChatAsync(string connectionId, string message, string friendConnectId)//1-ый парам. по 1-му заходу, 2-ой по 2-му
+        {
+            string alert = "";
+            //sender
+            var accountId = Session["accountId"] ?? 0;
+            if ((int)accountId == 0) {
+                return RedirectToAction("Login", "Accounts");
+            }
+            else {
+                ViewBag.User = null;
+                var sender = PushSender.InstanceClient;
+                AccountBO accountBO = DependencyResolver.Current.GetService<AccountBO>().Load((int)accountId);
+                if (accountBO != null) {
+                    Account = mapper.Map<AccountVM>(accountBO);
+                    sender.Account = Account;
+                    ViewBag.User = Account;
+                }
+                if (connectionId == "" || connectionId is null) {
+                    return View("Partial/_ChatAuctionView");
+                }
+                ViewBag.Actor = null;
+                message = message ?? "Hello friend!";
+                //addresser
+                if (friendConnectId != null && friendConnectId != "") {
+                    await sender.CommunicationWIthAuthor(message, connectionId, friendConnectId);
+                }
+                else {
+                    await sender.SendMessage(message); //SendCaller(message, connectionId);
+                }
+                
+                return new JsonResult { Data = alert, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
